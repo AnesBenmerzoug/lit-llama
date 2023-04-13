9ef601ab9335a66f13f48995068e442f379c63a0
Anes Benmerzoug <a.benmerzoug@appliedai.de>
2023-04-13 16:18:31 +0200

    Add support for forcing and resuming download

--- scripts/download_llama_hf_model.py
+++ scripts/download_llama_hf_model.py
@@ -7,12 +7,14 @@ from transformers import LlamaTokenizer, LlamaForCausalLM
 def download_hf_model(
     model_name: str = "decapoda-research/llama-7b-hf",
     model_dir: Path = Path("checkpoints/llama-7b-hf"),
+    force_download: bool = False,
+    resume_download: bool = True,
 ) -> None:
     print("Downloading model weights and tokenzier from pretrained LLaMA from HuggingFace")
 
-    LlamaTokenizer.from_pretrained(model_name, cache_dir=os.fspath(model_dir))
+    LlamaTokenizer.from_pretrained(model_name, cache_dir=os.fspath(model_dir), force_download=force_download, resume_download=resume_download)
 
-    LlamaForCausalLM.from_pretrained(model_name, cache_dir=os.fspath(model_dir))
+    LlamaForCausalLM.from_pretrained(model_name, cache_dir=os.fspath(model_dir), force_download=force_download, resume_download=resume_download)
 
 
 if __name__ == "__main__":
