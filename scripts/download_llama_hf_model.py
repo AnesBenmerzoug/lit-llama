0fc19156bf357d9d7f80fe9dd50ed3cbbba08261
Anes Benmerzoug <a.benmerzoug@appliedai.de>
2023-04-13 16:02:29 +0200

    Add script to download llama model from huggingface

---
+++ scripts/download_llama_hf_model.py
@@ -0,0 +1,21 @@
+import os
+from pathlib import Path
+
+from transformers import LlamaTokenizer, LlamaForCausalLM
+
+
+def download_hf_model(
+    model_name: str = "decapoda-research/llama-7b-hf",
+    model_dir: Path = Path("/mnt/data/llama_hf_models/llama-7b-hf"),
+) -> None:
+    print("Downloading model weights and tokenzier from pretrained LLaMA from HuggingFace")
+
+    LlamaTokenizer.from_pretrained(model_name, cache_dir=os.fspath(model_dir))
+
+    LlamaForCausalLM.from_pretrained(model_name, cache_dir=os.fspath(model_dir))
+
+
+if __name__ == "__main__":
+    from jsonargparse import CLI
+
+    CLI(download_hf_model)
